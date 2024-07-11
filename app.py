c0d2f6b1991993b1ad820db9fa90a2f0eb563f54
Daniel Guimaraes <daniel@gmail.com>
2024-07-11 09:29:12 -0300

    up

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 from middleware import authenticate_token
 from multiprocessing import Process, Value
 from flask import Flask, Response, render_template, jsonify
-import logging
 
 load_dotenv()
 app = Flask(__name__)
@@ -24,39 +23,83 @@ def monitoring(running):
     try:
         print('iniciou o monitoramento')
         cap = cv2.VideoCapture('rtsp://admin:admin@192.168.15.50:554/live/av0')
-        model = YOLO('/var/www/html/visionvortex.com.br/models/yolov8n.pt')
+        model = YOLO('/var/www/html/visionvortex.com.br/models/best.pt')
         skip_frames = 15
         current_frame = 0
-        ids = []
+        tinha_eating = []
+        tinha_drinking = []
+        tinha_in_box = []
+        lua_eating = []
+        lua_drinking = []
+        lua_in_box = []
+        threshold = 0.9
 
         while running.value:
-            print(running.value)
-            print('entrou no loop')
             success, frame = cap.read()
             if not success:
                 break
+            
+            frame_resized = cv2.resize(frame, (640,480))
 
             if current_frame % skip_frames == 0:
-                results = model.track(frame, persist=True, verbose=False, device='cuda')[0]
+                results = model.track(frame_resized, show=True, persist=True, verbose=False, device='cuda')[0]
                 if results:
                     for result in results:
                         if result.boxes and result.boxes.data.tolist():
                             detection = result.boxes.data.tolist()[0]
 
                             if len(detection) >= 7 and detection[6] == 0:
-                                if detection[5] > 0.75:
+                                if detection[5] > threshold:
                                     id = detection[4]
-                                    if id not in ids:
-                                        file_path = f'snapshots/alert_person.jpg'
-                                        cv2.imwrite(file_path, frame)
-                                        send_alert('Pessoa detectada', detection[5], file_path)
-                                        ids.append(id)
+                                    if id not in tinha_eating:
+                                        file_path = f'snapshots/alert_tinha_eating_{current_frame}.jpg'
+                                        cv2.imwrite(file_path, frame_resized)
+                                        tinha_eating.append(id)
+                            
+                            elif len(detection) >= 7 and detection[6] == 1:
+                                if detection[5] > threshold:
+                                    id = detection[4]
+                                    if id not in tinha_drinking:
+                                        file_path = f'snapshots/alert_tinha_drinking_{current_frame}.jpg'
+                                        cv2.imwrite(file_path, frame_resized)
+                                        tinha_drinking.append(id)
+
+                            elif len(detection) >= 7 and detection[6] == 2:
+                                if detection[5] > threshold:
+                                    id = detection[4]
+                                    if id not in tinha_in_box:
+                                        file_path = f'snapshots/alert_tinha_in_box_{current_frame}.jpg'
+                                        cv2.imwrite(file_path, frame_resized)
+                                        tinha_in_box.append(id)
+
+                            elif len(detection) >= 7 and detection[6] == 3:
+                                if detection[5] > threshold:
+                                    id = detection[4]
+                                    if id not in lua_eating:
+                                        file_path = f'snapshots/alert_lua_eating_{current_frame}.jpg'
+                                        cv2.imwrite(file_path, frame_resized)
+                                        lua_eating.append(id)
+                            
+                            elif len(detection) >= 7 and detection[6] == 4:
+                                if detection[5] > threshold:
+                                    id = detection[4]
+                                    if id not in lua_drinking:
+                                        file_path = f'snapshots/alert_lua_drinking_{current_frame}.jpg'
+                                        cv2.imwrite(file_path, frame_resized)
+                                        lua_drinking.append(id)
+
+                            elif len(detection) >= 7 and detection[6] == 5:
+                                if detection[5] > threshold:
+                                    id = detection[4]
+                                    if id not in lua_in_box:
+                                        file_path = f'snapshots/alert_lua_in_box_{current_frame}.jpg'
+                                        cv2.imwrite(file_path, frame_resized)
+                                        lua_in_box.append(id)
 
             current_frame += 1
-
-        print('saiu do loop')
-        cap.release()
-        cv2.destroyAllWindows()
+            cv2.imshow('Teste', frame_resized)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
     except Exception as e:
         print(f'Erro no processo de monitoramento: {e}')
@@ -151,7 +194,6 @@ def generate_frames():
             
 
 @app.route('/monitoring')
-@authenticate_token
 def monitoringCamera():
     return Response(generate_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
--- app.wsgi
+++ app.wsgi
@@ -2,7 +2,7 @@ import sys
 import logging
 
 sys.path.insert(0, '/var/www/html/visionvortex.com.br')
-sys.path.insert(0, '/var/www/html/visionvortex.com.br/venv/lib/python3.12/site-packages/')
+sys.path.insert(0, '/var/www/html/visionvortex.com.br/venv/lib/python3.10/site-packages/')
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
